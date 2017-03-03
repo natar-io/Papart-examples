fc1b310f481e47cea954e7997b5db5957b92693b
Jeremy Laviole <poqudrof@gmail.com>
2017-03-03 11:12:19 +0100

    repaired ruby example.

--- apps/ruby/camProjKinectExample.rb
+++ apps/ruby/camProjKinectExample.rb
@@ -3,11 +3,20 @@
 require 'jruby_art'
 require 'jruby_art/app'
 
-Processing::App::SKETCH_PATH = __FILE__
-Processing::App::load_library :PapARt, :javacv, :toxiclibscore, :SVGExtended
+# java_signature
+require 'jruby/core_ext'
 
+if not defined? Processing::App::SKETCH_PATH 
+  Processing::App::SKETCH_PATH = __FILE__  
+  Processing::App::load_library :PapARt, :javacv, :toxiclibscore, :SVGExtended
+end
+  
 module Papartlib
   include_package 'fr.inria.papart.procam'
+  include_package 'fr.inria.papart.procam.camera'
+  include_package 'fr.inria.papart.multitouch'
+  include_package 'fr.inria.papart.utils'
+  include_package 'import tech.lity.rea.svgextended'
 end
 
 class Sketch < Processing::App
@@ -15,38 +24,29 @@ class Sketch < Processing::App
   attr_reader :camera_tracking, :display, :papart, :moon
 
   def settings
-    size 200, 200, Processing::App::P3D
+    fullScreen Processing::App::P3D
+#    size 200, 200, Processing::App::P3D
   end
 
   def setup
+    @papart = Papartlib::Papart.projection(self)
+    @papart.loadTouchInput()
 
-    @useProjector = false
-
-    if @useProjector
-
-      @papart = PapartLib::Papart.projection(self)
-      @papart.loadTouchInput()
-    else
-      @papart = Papartlib::Papart.seeThrough self
-    end
-
-    @moon = PaperTouch.new
-
+    @paper = PaperTouch.new
     @papart.startTracking
+  end
 
-    def draw
-    end
+  def draw
   end
 end
 
 
-
-
-class PaperTouch < Papartlib::PaperScreen
+class PaperTouch < Papartlib::PaperTouchScreen
 
   def settings
     setDrawingSize 297, 210
     loadMarkerBoard(Papartlib::Papart::markerFolder + "A4-default.svg", 297, 210)
+
     setDrawOnPaper
   end
 
@@ -56,8 +56,10 @@ class PaperTouch < Papartlib::PaperScreen
   def drawOnPaper
     # @r = 100
     setLocation(0, 0, 0)
-    background @r, 0, 0
+    background 10
+    drawTouch 10
   end
+
 end
 
 Sketch.new unless defined? $app
